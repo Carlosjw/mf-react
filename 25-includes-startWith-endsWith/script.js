0813ee0bbe2cbcf733480a9165d17f48e8c18a9a
Carlosjw <carlos.ti.infor@gmail.com>
2023-01-09 00:07:15 -0300

    updateLessons

---
+++ 25-includes-startWith-endsWith/script.js
@@ -0,0 +1,10 @@
+// includes
+let names = ['Solange', 'Saymon', 'Livia', 'Carlos']
+
+console.log(names.includes("Carlos"))
+
+if(names.includes('Pedro')){
+    console.log('Está na lista.')
+}else{
+    console.log('Não está na lista')
+}
