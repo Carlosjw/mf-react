ea762a16122356a7d448d7893d4bb1f05810a71f
Carlosjw <carlos.ti.infor@gmail.com>
2023-01-22 00:08:50 -0300

    updateLessons

--- 26-react/03-praticando/src/App.js
+++ 26-react/03-praticando/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    // 'Pagar UNIMED Saymon',
+    // 'Estudar para concursos',
+    // 'Estudar ReactJS'
+  ]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]) // pegando todas as tarefas e adicionando mais uma no final
+    setInput(''); // limpando o campo do input após gravar a nova tarefa
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <h1>Cadastrando tarefas</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa</label><br />
+        <input type="text"
+          placeholder="Digite uma tarefa..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+         /><br /><br />
+
+         <button>Registrar</button>
+
+      </form>
+
+      <br /><br />
+
+      <ul>
+        {tarefas.map((tarefa, index) => ( // passano o parêntesis direto já estaremos retornando o valores
+          <li key={tarefa}>{`${index + 1}ª ${tarefa}`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
