e15e045463adb4be8aa2b829e98ae0af1fe48598
Carlosjw <carlos.ti.infor@gmail.com>
2023-02-08 00:20:29 -0300

    updateLesson

--- 26-react/04-useeffect/src/App.js
+++ 26-react/04-useeffect/src/App.js
@@ -1,9 +1,31 @@
 import { useState} from 'react';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Preparar meu filho pra escola',
+    'Me arrumar para ir para o trabalho',
+    'Fazer o meu trabalho como deve ser feito: com dedicação'
+  ])
+
+  function handleResgister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <li>Olá, eu mesmo</li>
+      <h1>Cadastro Tarefas</h1>
+
+      <form onSubmit={handleResgister}>
+        <label>Nome da tarefa:</label>
+        <input
+          placeholder='Digite uma tarefa...'
+        />
+      </form>
     </div>
   );
 }
